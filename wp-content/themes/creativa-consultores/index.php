9093d1fccbc29849afb5722bccb2c6f73096340e
Luis Chávez <luis.chavez@creativaconsultores.com>
2015-09-10 11:15:42 -0600

    Agregando descripcion de paquetes

--- wp-content/themes/creativa-consultores/index.php
+++ wp-content/themes/creativa-consultores/index.php
@@ -14,7 +14,7 @@
 get_header(); ?>
 		
 	<div id="primary" class="content-area col-md-12">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
